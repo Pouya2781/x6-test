03fbad6f0c2a032010c39ea81d425f75ac31d047
Pouya2781 <pouya.s2781@gmail.com>
2024-03-28 22:53:28 +0330

    [CHANGE] view template of custom node to better represent our data

--- src/app/custom-node/custom-node.component.ts
+++ src/app/custom-node/custom-node.component.ts
@@ -1,18 +1,25 @@
-import {Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {NgxPopperjsPlacements, NgxPopperjsTriggers} from 'ngx-popperjs';
 import {Element} from '@angular/compiler';
+import {CustomNodeService} from '../services/custom-node.service';
 
 export enum AccountType {
-    DEPOSIT = 'red',
-    CURRENT = 'green',
-    SAVINGS = 'blue',
+    DEPOSIT = 'سپرده',
+    CURRENT = 'جاری',
+    SAVINGS = 'پس انداز',
+}
+
+export enum NodeState {
+    NORMAL,
+    EXPANDED,
 }
 @Component({
     selector: 'app-custom-node',
     templateUrl: './custom-node.component.html',
     styleUrls: ['./custom-node.component.scss'],
 })
-export class CustomNodeComponent {
+export class CustomNodeComponent implements AfterViewInit {
+    @Input() public nodeId!: string;
     @Input() public transactionCount!: number;
     @Input() public ownerName!: string;
     @Input() public ownerFamilyName!: string;
@@ -23,7 +30,35 @@ export class CustomNodeComponent {
     @Input() public sheba!: string;
     @Input() public cardID!: string;
     @Input() public accountID!: string;
-    protected readonly NgxPopperjsPlacements = NgxPopperjsPlacements;
-    protected readonly NgxPopperjsTriggers = NgxPopperjsTriggers;
     protected readonly AccountType = AccountType;
+
+    @ViewChild('templateRef') templateRef!: ElementRef;
+
+    public nodeState: NodeState = NodeState.NORMAL;
+
+    constructor(
+        private customNodeService: CustomNodeService,
+        private changeDetector: ChangeDetectorRef
+    ) {}
+
+    ngAfterViewInit() {
+        const bounding = this.templateRef.nativeElement.getBoundingClientRect();
+        this.customNodeService.notify(this.nodeId, bounding.width, bounding.height);
+    }
+
+    showDetail() {
+        if (this.nodeState == NodeState.EXPANDED) this.nodeState = NodeState.NORMAL;
+        else this.nodeState = NodeState.EXPANDED;
+        this.changeDetector.detectChanges();
+        const bounding = this.templateRef.nativeElement.getBoundingClientRect();
+        this.customNodeService.notify(this.nodeId, bounding.width, bounding.height);
+    }
+
+    getBorderColor() {
+        if (this.accountType == AccountType.CURRENT) return 'red';
+        if (this.accountType == AccountType.DEPOSIT) return 'green';
+        return 'blue';
+    }
+
+    protected readonly NodeState = NodeState;
 }
